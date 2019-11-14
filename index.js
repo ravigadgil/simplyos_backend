e90bf7d38086d4d2b712be7b03e1d42be260506c
Redus Rira <redis123445@gmail.com>
2019-11-14 19:15:36 +0100

    aa

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ let Review = require('./models/Review');
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use('/pdf', express.static(__dirname + '/pathToPDF'));
